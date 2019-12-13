e1895b6509ce0c7b3aa9da75f374b59479a7d448
Kukundakwe Diana <52125203+KukundakweDiana@users.noreply.github.com>
2019-12-13 15:42:44 +0300

    Add files via upload

--- fizzBuzz.js
+++ fizzBuzz.js
@@ -1,23 +1,23 @@
 const array1 = [1, 2, 3, 4, 5, 6, 7, 8];
 const array2 = [1, 2, 3, 4];
-
+const array3 = array1.concat(array2);
 function fizzBuzz(array1, array2) {
-  const array3 = array1.concat(array2);
   if (array3.length % 3 === 0) {
-    return Fizz;
+    return "Fizz";
   }
 
   elseif(array3.length % 5 === 0);
   {
-    return Buzz;
+    return "Buzz";
   }
   elseif(array3.length % 5 === 0 && array3.length % 3 === 0);
   {
-    return FizzBuzz;
+    return "FizzBuzz";
   }
   elseif;
   {
     return 4;
   }
 }
-console.log(fizzBuzz(array2, array1));
+// console.log(fizzBuzz(array2, array1));
+module.exports = fizzBuzz;
