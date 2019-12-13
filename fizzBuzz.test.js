c3b6e335d285f71f2d47b03c962f984dab48fcdd
Kukundakwe Diana <52125203+KukundakweDiana@users.noreply.github.com>
2019-12-13 17:40:51 +0300

    Add files via upload

--- fizzBuzz.test.js
+++ fizzBuzz.test.js
@@ -1,31 +1,17 @@
 const fizzBuzz = require("./fizzBuzz");
 describe("this test should check the combined whether the array length is either divisible by 3, 5 or both", () => {
   it("checks that array length is divisible by 3", () => {
-    const array1 = [1, 2, 3, 4, 5, 6, 7, 8];
-    const array2 = [1, 2, 3, 4];
-    const array3 = array1.concat(array2);
-    expect(fizzBuzz(array3.length % 3 === 0)).toEqual("Fizz");
+    expect(fizzBuzz([1, 2, 3, 4, 5], [1, 2, 3, 4])).toBe("Fizz");
   });
   it("checks that arraylength is completely divisible by 5", () => {
-    const array1 = [1, 2, 3, 4, 5, 6, 7, 8];
-    const array2 = [1, 2, 3, 4];
-    const array3 = array1.concat(array2);
-    expect(fizzBuzz(array3.length % 5 === 0)).toEqual("Buzz");
+    expect(fizzBuzz([1, 2, 3, 4, 5], [])).toBe("Buzz");
   });
   it("checks that arraylenth is divisible by both 3 and 5 leaving no remainder", () => {
-    const array1 = [1, 2, 3, 4, 5, 6, 7, 8];
-    const array2 = [1, 2, 3, 4];
-    const array3 = array1.concat(array2);
-    expect(
-      fizzBuzz(array3.length % 5 === 0 && array3.length % 3 === 0)
-    ).toEqual("FizzBuzz");
+    expect(fizzBuzz([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], [1, 2, 3, 4, 5])).toBe(
+      "FizzBuzz"
+    );
   });
   it("checks that anything that returns a remainder returns 4", () => {
-    const array1 = [1, 2, 3, 4, 5, 6, 7, 8];
-    const array2 = [1, 2, 3, 4];
-    const array3 = array1.concat(array2);
-    expect(
-      fizzBuzz(array3.length % 5 !== 0 && array3.length % 3 !== 0).toEqual(4)
-    );
+    expect(fizzBuzz([1], [2]).toBe(4));
   });
 });
