f59c1502415556a22217c31350d0ae90005d6a5d
Kukundakwe Diana <52125203+KukundakweDiana@users.noreply.github.com>
2019-12-17 11:29:11 +0300

    Add files via upload

--- fizzBuzz.js
+++ fizzBuzz.js
@@ -1,11 +1,10 @@
-const array1 = [1, 2, 3, 4, 5, 6, 7, 8];
+const array1 = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 const array2 = [1, 2, 3, 4];
 const array3 = array1.concat(array2);
 function fizzBuzz(array1, array2) {
   if (array3.length % 3 === 0) {
     return "Fizz";
   }
-
   elseif(array3.length % 5 === 0);
   {
     return "Buzz";
@@ -18,7 +17,8 @@ function fizzBuzz(array1, array2) {
   {
     return 4;
   }
-  return array3;
+  return array2;
+  return array1;
 }
 // console.log(fizzBuzz(array2, array1));
 module.exports = fizzBuzz;
--- fizzBuzz.test.js
+++ fizzBuzz.test.js
@@ -1,7 +1,7 @@
 const fizzBuzz = require("./fizzBuzz");
 describe("this test should check the combined whether the array length is either divisible by 3, 5 or both", () => {
   it("checks that array length is divisible by 3", () => {
-    expect(fizzBuzz([1, 2, 3, 4, 5], [1, 2, 3, 4])).toBe("Fizz");
+    expect(fizzBuzz([1, 2, 3, 4, 5, 6, 7, 8], [1, 2, 3, 4])).toBe("Fizz");
   });
   it("checks that arraylength is completely divisible by 5", () => {
     expect(fizzBuzz([1, 2, 3, 4, 5], [])).toBe("Buzz");
@@ -12,6 +12,6 @@ describe("this test should check the combined whether the array length is either
     );
   });
   it("checks that anything that returns a remainder returns 4", () => {
-    expect(fizzBuzz([1], [2]).toBe(4));
+    expect(fizzBuzz([1, 2, 3], [1]).toBe(4));
   });
 });
